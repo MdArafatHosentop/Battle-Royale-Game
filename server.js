b2302a0834c9be365e7fdfe731c6c7d45e703972
Md Arafat Hosen <djarafathd@gmail.com>
2026-02-25 04:21:53 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
+app.use(express.static("../client"));
+
+let players = {};
+
+io.on("connection", socket=>{
+  players[socket.id] = {x:0,y:0,z:0};
+
+  socket.on("move", data=>{
+    players[socket.id] = data;
+    io.emit("players", players);
+  });
+
+  socket.on("disconnect", ()=>{
+    delete players[socket.id];
+  });
+});
+
+http.listen(process.env.PORT || 3000, ()=>{
+  console.log("Server Running");
+});
