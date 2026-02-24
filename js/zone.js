ba37bba89f29440a96be63a05a757ad07a4e1aab
Md Arafat Hosen <djarafathd@gmail.com>
2026-02-25 04:05:35 +0600

    Update zone.js

--- js/zone.js
+++ js/zone.js
@@ -1,1 +1,10 @@
+let zoneSize = 200;
 
+function updateZone(){
+    setInterval(()=>{
+        zoneSize -= 5;
+        if(Math.abs(player.position.x) > zoneSize){
+            alert("Outside Safe Zone!");
+        }
+    },5000);
+}
