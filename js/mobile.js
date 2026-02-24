9bb60291b9fbac8a97e963d4fe84f3e5fe482ca9
Md Arafat Hosen <djarafathd@gmail.com>
2026-02-25 04:16:04 +0600

    Delete js directory

--- js/mobile.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("touchmove", function(){
-    if(player){
-        player.position.x += 0.1;
-    }
-});
--- js/player.js
+++
@@ -1,8 +0,0 @@
-let player;
-function createPlayer(scene){
-    let geo = new THREE.BoxGeometry(2,2,2);
-    let mat = new THREE.MeshBasicMaterial({color: 0x0000ff});
-    player = new THREE.Mesh(geo, mat);
-    player.position.y = 1;
-    scene.add(player);
-}
