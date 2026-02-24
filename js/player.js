94429a3d85d910e0dd2a1b4e8cd79940c18cb01e
Md Arafat Hosen <djarafathd@gmail.com>
2026-02-25 04:04:57 +0600

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,1 +1,8 @@
-
+let player;
+function createPlayer(scene){
+    let geo = new THREE.BoxGeometry(2,2,2);
+    let mat = new THREE.MeshBasicMaterial({color: 0x0000ff});
+    player = new THREE.Mesh(geo, mat);
+    player.position.y = 1;
+    scene.add(player);
+}
